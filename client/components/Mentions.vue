d171eda2178dd6a79b0cd89f9ca1ef15f04c1729
Pavel Djundik <xPaw@users.noreply.github.com>
2020-01-17 12:01:46 +0200

    Move css to component

--- client/components/Mentions.vue
+++ client/components/Mentions.vue
@@ -42,6 +42,64 @@
 	</div>
 </template>
 
+<style>
+.mentions-popup {
+	background-color: var(--window-bg-color);
+	position: absolute;
+	width: 400px;
+	right: 80px;
+	top: 55px;
+	max-height: 400px;
+	overflow-y: scroll;
+	z-index: 2;
+	padding: 10px;
+}
+
+.mentions-popup > .mentions-popup-title {
+	margin-bottom: 10px;
+	font-size: 20px;
+}
+
+.mentions-popup .msg {
+	margin-bottom: 15px;
+	user-select: text;
+}
+
+.mentions-popup .msg:last-child {
+	margin-bottom: 0;
+}
+
+.mentions-popup .msg .content {
+	background-color: var(--highlight-bg-color);
+	border-radius: 5px;
+	padding: 6px;
+	margin-top: 2px;
+}
+
+.mentions-popup .msg-hide::before {
+	font-size: 20px;
+	font-weight: normal;
+	display: inline-block;
+	line-height: 16px;
+	text-align: center;
+	content: "×";
+}
+
+@media (max-width: 768px) {
+	.mentions-popup {
+		border-radius: 0;
+		border: 0;
+		box-shadow: none;
+		width: 100%;
+		max-height: none;
+		right: 0;
+		left: 0;
+		bottom: 0;
+		top: 45px; /* header height */
+	}
+}
+</style>
+
 <script>
 import Username from "./Username.vue";
 import ParsedMessage from "./ParsedMessage.vue";
